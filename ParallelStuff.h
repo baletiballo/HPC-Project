5dac94a81514cb1b707038e5069d079a3a457b07
Gebhart97 <stefan.gebhart1@web.de>
2021-06-24 17:09:15 +0200

    Some documentation on what methods do

--- ParallelStuff.h
+++ ParallelStuff.h
@@ -19,7 +19,7 @@
 
 using namespace std;
 
-class JobQueue {
+class JobQueue { //stores packaged Jobs for our threads to complete
 public:
 	mutex jobQueueMutex;
 	queue<packaged_task<void()>> jobQueue;
@@ -27,22 +27,22 @@ public:
 	bool abort;
 
 	JobQueue();
-	void push(packaged_task<void()> job);
-	packaged_task<void()> pop();
-	void terminate();
+	void push(packaged_task<void()> job); //add job to queue
+	packaged_task<void()> pop(); //remove first job in queue and returns it
+	void terminate(); //empties the queue and makes threads return ending them
 };
 
-class ThreadPool {
+class ThreadPool { //thread pool of worker threads, getting jobs from the queue
 public:
 	int threads;
 	vector<thread> pool;
 	JobQueue queue;
 
 	ThreadPool(int numThreads);
-	void threadsDoWork();
+	void threadsDoWork(); //thread method, making them get and complete jobs in an infinite loop
 };
 
-class Sem {
+class Sem { //standard semaphore
 public:
 	mutex mtx;
 	condition_variable cv;
@@ -50,21 +50,21 @@ public:
 
 	Sem(int countInit);
 
-	void V(int n);
+	void V(int n); //add n to count and wakeUp everyone that waits in P()
 
-	void P(int n);
+	void P(int n); //wait until count at least n, then subtract n
 
-	void set(int n);
+	void set(int n); //set count to n, CAUTION: don't do this while someone is waiting in P(), as they won't wake up from this, use V() for that instead
 };
 
 ///////////
 
-extern ThreadPool pool;
+extern ThreadPool pool; //our shared Thread Pool
 
-void endThreads();
+void endThreads(); //make threads end
 
-void pushJob(packaged_task<void()> job);
+void pushJob(packaged_task<void()> job); //push a new job to the queue for the threads to complete
 
-extern Sem sem;
+extern Sem sem; //our shared Semaphore for Parallelization
 
 #endif /* PARALLELSTUFF_H_ */
